940a230f2ce69b86d955d8760effa6341ea4589d
LucasPlencovich <plencovich1@gmail.com>
2023-06-11 18:57:58 -0300

    Actividad de Laboratorio-AyB#1

--- Promedio/Promedio/Program.cs
+++ Promedio/Promedio/Program.cs
@@ -10,7 +10,40 @@ namespace Promedio
     {
         static void Main(string[] args)
         {
+            Promedio mi_promedio = new Promedio();
+            int opcion = 0, num;
+            do 
+            {
+                Console.Clear();
+                Console.WriteLine("1. Ingresar numero");
+                Console.WriteLine("2. Mostrar promedio");
+                Console.WriteLine("Otro. salir");
+                Console.Write("Opcion: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
 
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            Console.Write("Ingrese un numero: ");
+                            num = Convert.ToInt32(Console.ReadLine());
+                            mi_promedio.Agregar_valor(num);
+                        }break;
+                    case 2:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("El promedio es: {0}", mi_promedio.Calcuar_promedio());
+                            Console.ReadKey();
+                        }break;
+
+                    default:
+                        {
+                            opcion = 0;
+                        }break;
+                }
+
+            } while (opcion != 0);
         }
     }
 }
---
+++ Promedio/Promedio/Promedio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio
+{
+    class Promedio
+    {
+        private double valores;
+        private int cant_num;
+        public Promedio()
+        {
+        }
+        public void Agregar_valor(int num)
+        {
+            valores += num;
+            cant_num++;
+        }
+
+        public double Calcuar_promedio()
+        {
+            double resultado = 0;
+
+            if (cant_num == 0)
+            {
+                resultado = 0;
+            }
+
+            resultado = valores / cant_num;
+            return resultado;
+        }
+    }
+}
