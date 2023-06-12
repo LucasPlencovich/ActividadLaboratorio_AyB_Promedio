2ba827c9640db41043c3e7cf4e6fa9879ce9235c
LucasPlencovich <plencovich1@gmail.com>
2023-06-11 21:45:20 -0300

    Actividad de Laboratorio-AyB#4

--- Promedio/Promedio/Program.cs
+++ Promedio/Promedio/Program.cs
@@ -10,6 +10,10 @@ namespace Promedio
     {
         static void Main(string[] args)
         {
+            /*Para oredenar el vector utilice el metodo burbuja, ya que no termine de entender el funcionamiento 
+             del metodo quicksort y no queria solo copiar y pegar el codigo.
+            Tambien provando el codigo me di cuenta que para que muestre el tamaño del vector (cantidad de valores que
+            superan al promedio) debe ejecutarce antes la opcion 3*/
             Promedio mi_promedio = new Promedio();
             int opcion = 0, num, cont=0;
             do 
@@ -18,6 +22,8 @@ namespace Promedio
                 Console.WriteLine("1. Ingresar numero");
                 Console.WriteLine("2. Mostrar promedio");
                 Console.WriteLine("3. Valores que superan el promedio");
+                Console.WriteLine("4. Mostrar cantidad de valores que superan al promedio");
+                Console.WriteLine("5. Valores que superan al promedio ordenados");
                 Console.WriteLine("Otro. salir");
                 Console.Write("Opcion: ");
                 opcion = Convert.ToInt32(Console.ReadLine());
@@ -46,6 +52,24 @@ namespace Promedio
                                 if (mi_promedio.Val_sup_prom(i) != 0)
                                 {
                                     Console.WriteLine("{0:f2}", mi_promedio.Val_sup_prom(i));
+                                }
+                            }
+                            Console.ReadKey();
+                        }break;
+                    case 4:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("{0}", mi_promedio.tamaño_vec);
+                            Console.ReadKey();
+                        }break;
+                    case 5:
+                        {
+                            Console.Clear();
+                            for (int i = 0; i < cont; i++)
+                            {
+                                if (mi_promedio.Val_sup_prom(i) != 0)
+                                {
+                                    Console.WriteLine("{0:f2}", mi_promedio.vec_ordenado(i));
                                 }
                             }
                             Console.ReadKey();
--- Promedio/Promedio/Promedio.cs
+++ Promedio/Promedio/Promedio.cs
@@ -9,12 +9,17 @@ namespace Promedio
     class Promedio
     {
         private double [] valores;
+        private int tam_vec;
         private int cant_num;
         private double promedio;
         public Promedio()
         {
             valores = new double[100];
             cant_num = 0;
+        }
+        public int tamaño_vec
+        {
+            get{ return tam_vec; }
         }
         public void Agregar_valor(int num)
         {
@@ -40,18 +45,41 @@ namespace Promedio
         }
         public double Val_sup_prom(int num)
         {
-            double[] val_sup_prom = new double[100];
+            double[] val_sup_prom = new double[cant_num];
             int pos = 0;
-            
             for (int i = 0; i < cant_num; i++)
             {
                 if (valores[i] > promedio)
                 {
                     val_sup_prom[pos] = valores[i];
                     pos++;
+                    tam_vec = pos;
                 }
             }
             return val_sup_prom[num];
+        }
+
+        public double vec_ordenado(int num)
+        {
+            double[] val_sup_prom = new double[cant_num];
+            for (int i = 0; i < cant_num; i++)
+            {
+                val_sup_prom[i] = Val_sup_prom(i);
+            }
+            double aux = 0;
+            for(int i = 0; i < tam_vec; i++)
+            {
+                for (int j = 1; j < tam_vec; j++)
+                {
+                    if (val_sup_prom[i] > val_sup_prom[j])
+                    {
+                        aux = val_sup_prom[j];
+                        val_sup_prom[j] = val_sup_prom[i];
+                        val_sup_prom[i] = aux;
+                    }
+                }
+            }
+            return  val_sup_prom[num];
         }
     }
 }
